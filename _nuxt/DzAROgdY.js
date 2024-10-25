2339c817a6a6a31b3cc3b46eb3d86df025275258
Rafael Leonan <faelleonan@gmail.com>
2024-10-25 20:40:09 -0300

    Updates

--- _nuxt/DzAROgdY.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./DtrVyelK.js";import{f as t,_ as o,c as n,o as a}from"./BlSa1Ebj.js";const s=t({name:"Contacts",methods:{defineHead(){e({title:"Contatos"})}},mounted(){this.defineHead()}});function c(r,d,f,i,m,p){return a(),n("h1",null,"Tela de Contatos")}const u=o(s,[["render",c]]);export{u as default};
