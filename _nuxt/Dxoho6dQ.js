ebde529b64a2701c458dee35025fd6b1d7b2658c
Rafael Leonan <faelleonan@gmail.com>
2024-10-25 21:21:50 -0300

    Updates

---
+++ _nuxt/Dxoho6dQ.js
@@ -0,0 +1,1 @@
+import{u as e}from"./BRTX-6cT.js";import{f as o,_ as t,c as s,o as n}from"./oFxRb40W.js";const r=o({name:"Projects",methods:{defineHead(){e({title:"Projetos"})}},mounted(){this.defineHead()}});function a(c,d,f,m,p,i){return n(),s("h1",null,"Projetos - Tela em construção...")}const l=t(r,[["render",a]]);export{l as default};
