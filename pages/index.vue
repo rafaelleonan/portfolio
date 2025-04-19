cd04b2d195afe9c7334e55c49a2a5ac0f5749c8d
Rafael Leonan <faelleonan@gmail.com>
2025-04-19 15:06:16 -0300

    Ajustes

--- pages/index.vue
+++ pages/index.vue
@@ -28,21 +28,21 @@ useHead({
       <span class="title-section">SOBRE MIM</span>
       <div class="app-card">
         <div class="card-body">
-           <span>
+           <span class="text-spacing-1">
              Sou desenvolvedor full stack com experiência em desenvolvimento de aplicações back-end: APIs RESTful,
-             serviços agendados (Cron Service), Webhooks e serviços de mensageria Kafka e AWS SQS, aplicações frond-end: mobile, web e desktop. Também possuo
+             serviços agendados (Cron Service), Webhooks e serviços de mensageria Kafka e AWS SQS, aplicações frond-end: mobile, web e desktop. Tenho
              conhecimento em armazenamento e leitura de dados com AWS S3, Firebase Firestore, além de banco de dados relacionais
-             como MySQL e PostgreSQL. Também possuo experiência com Firebase Authentication e Storage.
+             como MySQL e PostgreSQL. Possuo experiência com Firebase Authentication e Storage em projetos pessoais.
            </span>
         </div>
       </div>
     </section>
     <section class="section-default app-mt-2">
       <span class="title-section">STACK ATUAL</span>
-      <div class="stack-current">
+      <div class="stack">
         <div class="tech-stack" v-for="(stack, sKey) in stackCurrent" :key="`stack_current_${sKey}`">
           <span>{{ stack.title }}</span>
-          <img :src="stack.src"/>
+          <img :src="stack.src" :alt="stack.title"/>
         </div>
       </div>
     </section>
@@ -129,13 +129,12 @@ useHead({
              v-for="(tech, techKey) in MockTechnologies"
              :key="`tech-${techKey}`">
           <span class="title-card-experience"><div class="divisor"></div>{{ tech.title }}<div class="divisor"></div></span>
-          <div class="badge-xxl badge-blue-dark-2"
-               v-for="(line, lineKey) in tech.badges"
-               :key="`line-${lineKey}`">
-            <div class="icon">
-              <img :src="line.src" :alt="line.title" :width="line.title === 'JAVA' ? '25px' : '15px'"/>
+          <div class="stack">
+            <div class="tech-stack all-techs" v-for="(line, lineKey) in tech.badges"
+                 :key="`line-${lineKey}`">
+              <span>{{ line.title }}</span>
+              <img :src="line.src" :alt="line.title"/>
             </div>
-            <span>{{ line.title }}</span>
           </div>
         </div>
       </div>
