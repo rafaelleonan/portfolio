b08478a25152bd87b56d6202f8f8e3f06842e516
Rafael Leonan <faelleonan@gmail.com>
2025-06-29 22:05:04 -0300

    Ajuste

--- plugins/firebase.client.ts
+++ plugins/firebase.client.ts
@@ -2,17 +2,19 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
+import {useRuntimeConfig} from "#imports";
+import {process} from "std-env";
 
 export default defineNuxtPlugin(() => {
 	const config = useRuntimeConfig()
 	
 	const firebaseConfig = {
-		apiKey: config.public.firebaseApiKey,
-		authDomain: config.public.firebaseAuthDomain,
-		projectId: config.public.firebaseProjectId,
-		storageBucket: config.public.firebaseStorageBucket,
-		messagingSenderId: config.public.firebaseMessagingSenderId,
-		appId: config.public.firebaseAppId,
+		apiKey: config.public.firebaseApiKey ?? process.env.NUXT_PUBLIC_FIREBASE_API_KEY,
+		authDomain: config.public.firebaseAuthDomain ?? process.env.NUXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+		projectId: config.public.firebaseProjectId ?? process.env.NUXT_PUBLIC_FIREBASE_PROJECT_ID,
+		storageBucket: config.public.firebaseStorageBucket ?? process.env.NUXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+		messagingSenderId: config.public.firebaseMessagingSenderId ?? process.env.NUXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+		appId: config.public.firebaseAppId ?? process.env.NUXT_PUBLIC_FIREBASE_APP_ID,
 	}
 	
 	const app = initializeApp(firebaseConfig)
