d7f032ad8e7de9a7515ddf50dea2358cb88b0d78
Rafael Leonan <faelleonan@gmail.com>
2025-06-30 19:27:03 -0300

    Ajuste

--- plugins/firebase.client.ts
+++ plugins/firebase.client.ts
@@ -5,17 +5,21 @@ import { getFirestore } from 'firebase/firestore'
 import {useRuntimeConfig} from "#imports";
 import {process} from "std-env";
 
-export default defineNuxtPlugin(() => {
-	const config = useRuntimeConfig()
-	
-	const firebaseConfig = {
-		apiKey: config.public.firebaseApiKey ?? process.env.NUXT_PUBLIC_FIREBASE_API_KEY,
-		authDomain: config.public.firebaseAuthDomain ?? process.env.NUXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-		projectId: config.public.firebaseProjectId ?? process.env.NUXT_PUBLIC_FIREBASE_PROJECT_ID,
-		storageBucket: config.public.firebaseStorageBucket ?? process.env.NUXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-		messagingSenderId: config.public.firebaseMessagingSenderId ?? process.env.NUXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-		appId: config.public.firebaseAppId ?? process.env.NUXT_PUBLIC_FIREBASE_APP_ID,
-	}
+export default defineNuxtPlugin((nuxtApp) => {
+	let firebaseConfig: any
+	nuxtApp.hook('app:mounted', () => {
+		const config = useRuntimeConfig()
+		console.log('Config app:mounted: ', config)
+		firebaseConfig = {
+			apiKey: config.public.firebaseApiKey ?? process.env.NUXT_PUBLIC_FIREBASE_API_KEY,
+			authDomain: config.public.firebaseAuthDomain ?? process.env.NUXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+			projectId: config.public.firebaseProjectId ?? process.env.NUXT_PUBLIC_FIREBASE_PROJECT_ID,
+			storageBucket: config.public.firebaseStorageBucket ?? process.env.NUXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+			messagingSenderId: config.public.firebaseMessagingSenderId ?? process.env.NUXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+			appId: config.public.firebaseAppId ?? process.env.NUXT_PUBLIC_FIREBASE_APP_ID,
+		}
+	})
+	console.log('firebaseConfig: ', firebaseConfig)
 	
 	const app = initializeApp(firebaseConfig)
 	
